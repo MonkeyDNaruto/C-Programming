fba4452dd8cda45c886ef7e4d844098019f39605
NischalShrestha <nischalshrestha324@gmain.com>
2023-06-15 09:55:52 +0545

    scan

---
+++ scan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    char s[30];
+    printf("i: \n");
+    scanf("%i", &i);
+    printf("i is: %i \n", i);
+    printf("s:\n");
+    scanf("%s", s);
+    printf("s is: %s\n", s);
+}
